0485afab0ec6547b85aaf15616acb81940d4da86
Andy Yong <andyywz@gmail.com>
2017-04-08 13:42:42 -0400

    add uniqueness tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,6 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:login) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_uniqueness_of(:login).case_insensitive }
 end
