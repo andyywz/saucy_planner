46d5fac1e93a45231916d8798e2c34d939642671
Andy Yong <andyywz@gmail.com>
2017-04-08 13:10:55 -0400

    fix migration

--- db/migrate/20170408170633_create_events.rb
+++ db/migrate/20170408170633_create_events.rb
@@ -4,7 +4,7 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.string :name
       t.datetime :date
       t.datetime :end_date
-      t.references :user_id, foreign_key: true
+      t.references :user, foreign_key: true
       t.text :description
 
       t.timestamps
